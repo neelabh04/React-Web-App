2fa4f91f8a44bd8f8d26e02a599b105c78e28981
Neelabh Singh <neelabh041997@gmail.com>
2022-10-29 15:47:03 +0530

    Initial Build

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <Footer>
+        <div>
+            <h1>Techy-Star</h1>
+            <p>@ all rights reserved</p>
+        </div>
+
+        <div>
+            <h5>Follow Us</h5>
+            <div>
+                <a href="https://www.youtube.com/" target={"blank"}>Youtube</a>
+                <a href="https://instagram.com/" target={"blank"}>Instagram</a>
+                <a href="https://github.com/neelabh04" target={"blank"}>Github</a>
+            </div>
+        </div>
+    </Footer>
+  )
+}
+
+export default Footer
